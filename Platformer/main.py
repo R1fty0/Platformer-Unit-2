36db0269f7aa26394a830a35d5e9b2fed5e8dd71
R1fty0 <113545024+R1fty0@users.noreply.github.com>
2022-11-07 21:01:25 -0800

    This code is used to spawn the player in, and the rect function updates the player's position automatically. However, the appearance looks quite jittery. May need a small rework.
     # Player spawn position.
        PlayerSpawnX = Width / 4
        PlayerSpawnY = Height / 2
    
        # Displays the Player at the location listed above.
        Player = pygame.Rect(PlayerSpawnX, PlayerSpawnY, PlayerWidth, PlayerHeight)

--- Platformer/main.py
+++ Platformer/main.py
@@ -1,47 +1,113 @@
-# Time left to work on Project: 26h 00m
+# Time left to work on Project: 25h
 
 # Imports
 import pygame
-from images import WalkForwardImages
-from images import Environment
-
-
-# Instantiation of Classes
-WalkImagesList = WalkForwardImages()
-Background = Environment()
+import os
 
 # Refresh Rate of Program
 FPS = 60
 
 # Dimensions of Game Window
-Width, Height = 1000, 500
+Width, Height = 800, 600
 
 # Player Dimensions
 PlayerWidth, PlayerHeight = 144, 194
+# Player Speed
+Speed = 10
 
-# Scales all images to the correct dimensions
-WalkImagesList.scale(PlayerWidth, PlayerHeight)
+
+# Scales Images
+def ScaleImages(List, Width, Height):
+    for i in List:
+        pygame.transform.scale(i, (Width, Height))
+
+
+# Walking Animation
+WalkForward = [pygame.image.load(os.path.join("venv", "p1_walk01.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk02.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk03.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk04.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk05.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk06.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk07.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk08.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk09.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk10.png")),
+               pygame.image.load(os.path.join("venv", "p1_walk11.png"))]
+
+print("The amount of frames in the Walking Animation loop is " f"{len(WalkForward)}.")
+
+# Background
+Background = pygame.image.load(os.path.join("venv", "background.png"))
+# Idle
+Idle = pygame.image.load(os.path.join("venv", "p1_stand.png"))
+# Jumping
+Jumping = pygame.image.load(os.path.join("venv", "p1_jump.png"))
+
+# Scales Images to a desired size.
+ScaleImages(WalkForward, PlayerWidth, PlayerHeight)
 
 # Creates Window and Name of the Game
 Window = pygame.display.set_mode((Width, Height))
 pygame.display.set_caption("Platformer")
 
 
+"""
+
+This is a border between the functions and the other pieces of code. Realistically, the code above should be implemented into various data structures.
+
+"""
+
+
+
+
+
+# Updates the Visuals on the Screen.
+def Canvas(Player):
+
+    # Draws Background
+    Window.blit(Background, (0, 0))
+    # Draws Player in the idle state
+    Window.blit(Idle, (Player.x, Player.y))
+
+    # Updates the Screen with the changes made by this function.
+    pygame.display.update()
+
+
+def Movement(Player):
+    KeyPressed = pygame.key.get_pressed()
+
+    if KeyPressed[pygame.K_a]:
+        # Moves Player to the Right, or Forward relative to the game's perspective.
+        Player.x -= Speed
+    if KeyPressed[pygame.K_d]:
+        # Moves Player to the Left, or Backwards relative to the game's perspective.
+        Player.x += Speed
+    if KeyPressed[pygame.K_w]:
+        # Allows the Player to jump
+        Player.y -= Speed
+
+
 def Update():
-    # Gets the Background from the Environment Class
-    image = Background.getBackground
-    # Draws Background on Screen
-    Window.blit(image, (0, 0))
+    # Player spawn position.
+    PlayerSpawnX = Width / 4
+    PlayerSpawnY = Height / 2
+
+    # Displays the Player at the location listed above.
+    Player = pygame.Rect(PlayerSpawnX, PlayerSpawnY, PlayerWidth, PlayerHeight)
+
     # Clock that maintains FPS
     clock = pygame.time.Clock()
     # If this boolean is true, then the program runs.
     run = True
     while run:
         clock.tick(FPS)
-        # Loops through this check until the user decides to quit the program, then the code in the loop will run
+        # Terminates Program if the User Closes the Application
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            Movement(Player)
+            Canvas(Player)
     pygame.quit()
 
 
